4c7474bad6433e12eb98bc02efb7896b7434a73c
Noah <burghardtnoah@gmail.com>
2023-03-02 03:56:45 +0000

    added some more PMM collections

--- docker-media/plex_meta_manager/bootstrap.py
+++ docker-media/plex_meta_manager/bootstrap.py
@@ -12,6 +12,18 @@ for var in set(re.findall(r'\${([A-Za-z_]+)}', s)):
     print(f'replacing ${{{var}}} with {val}')
     s = s.replace(f'${{{var}}}', val)
 
+
+# regex to keep existing trakt auth data
+p = r'(trakt:[\n ]*\n(?:\s{2}[#A-Za-z][^\n]+(?:[\n ]*\n))*)(\s{2}authorization:[\n ]*?(?:[\n ]*\n {4}[#A-Za-z][^\n]+)*)'
+of = ''
+with open('/data/output') as f:
+    of = f.read()
+if re.search(p, of) and re.search(p, s):
+    go = re.search(p, of).groups()
+    gi = re.search(p, s).groups()
+    if len(go) == 2 and len(gi) == 2:
+        s = re.sub(p, gi[0] + go[1], s)
+
 with open('/data/output', 'w') as f:
     f.write(s)
 
